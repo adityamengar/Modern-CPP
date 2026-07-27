30aeb165ec7759302bd7e31b4311667f6bfc001f
adityamengar <mengaraditya@gmail.com>
2026-07-27 22:52:40 +0530

    Add Numbers

--- io_loops/add_2_number.cpp
+++ io_loops/add_2_number.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {int a,b;
 cout<<"Enter 2 Numbers: ";
 cin>>a>>b;
-int sum=a+b;
+int sum=a+b;//Addition of 2 numbers
 cout<<"Sum of "<<a<<"and "<<b<<"is: "<<sum<<endl;
 }
