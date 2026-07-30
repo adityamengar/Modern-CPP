f4fc48fa3f1a2b2b2c1768caf0df50f7eb6756e7
adityamengar <mengaraditya@gmail.com>
2026-07-30 23:01:36 +0530

    Vector 2

--- array_strings/vector.cpp
+++ array_strings/vector.cpp
@@ -2,3 +2,14 @@
 #include<vector>
 using namespace std;
 #define MAX 100
+
+int main(){ vector<int> arr(MAX);
+cout<<"Enter the no. of elements: ";
+int count, sum=0;
+cin>>count;
+for(int i=0; i<count; i++){
+    arr[i]=i; sum += arr[i];
+    }
+cout<<"Array Sum: "<<sum<<endl;
+return 0;
+}
