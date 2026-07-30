ff7f59d4fb22822ec00565d64d0de193dc48a395
adityamengar <mengaraditya@gmail.com>
2026-07-30 19:35:13 +0530

    Fixed sized array basic

---
+++ array_strings/fixed_size_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    short age[4];
+
+    age[0]=10;
+    age[1]=20;
+    age[2]=18;
+    age[3]=27;
+    
+    cout<<age[0]<<" "<<endl;
+    cout<<age[1]<<" "<<endl;
+    cout<<age[2]<<" "<<endl;
+    cout<<age[3]<<" "<<endl;
+
+    return 0;
+}
