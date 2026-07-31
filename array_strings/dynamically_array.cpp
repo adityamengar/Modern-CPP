8e01351e87012310331384985582a2aa4f58cb1e
adityamengar <mengaraditya@gmail.com>
2026-07-31 23:43:32 +0530

    Dynamically arrange sized arrays

---
+++ array_strings/dynamically_array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    cout<<"Enter The No. Of Elements: ";
+    int count, sum=0;
+    cin>>cout;
+
+    vector<int> arr;
+    
+}
