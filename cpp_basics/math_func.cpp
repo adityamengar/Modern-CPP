443cc5107bdacd37155620115f3eedae29aec213
adityamengar <mengaraditya@gmail.com>
2026-08-04 23:59:00 +0530

    ALL FUNCTIONS ADDED

--- cpp_basics/math_func.cpp
+++ cpp_basics/math_func.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main(){
-    double x = 3;
+    double x = 3.14;
     double y = 4;
     double z;
 
-    //z=max(x,y);
-    //z=min(x,y);
-    z=pow(2,4);
-
+    //z=max(x,y);  //maximum
+    //z=min(x,y); //minimum
+    //z=pow(2,4); //power
+    //z=sqrt(9); //square root
+    //z=abs(-3); //absolute value nearest to zero how far is it so output is 3
+    //z=round(x); //rounding off the number to nearest integer
+    //z= ceil(x); //rounding up the number to nearest integer
+    z=floor(x);
     cout<<z<<endl;
 }
