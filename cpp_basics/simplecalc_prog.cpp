554ff5935ad1abfbc82c00e8ee9bae4218ff8fd2
adityamengar <mengaraditya@gmail.com>
2026-08-10 23:30:18 +0530

    Calc Program with switch statement

--- cpp_basics/simplecalc_prog.cpp
+++ cpp_basics/simplecalc_prog.cpp
@@ -8,5 +8,34 @@ int main(){
     double result;
     cout<<"-----CALCULATOR-----"<<endl;
     cout<<"Enter either(+,-,*,/) : ";
-    cin>>op
+    cin>>op;
+    cout<<"Enter #1: ";
+    cin>>num1;
+    cout<<"Enter #2: ";
+    cin>>num2;
+
+    switch(op){
+        case '+':
+            result=num1+num2;
+            cout<<"Result: "<<result<<endl;
+            break;
+        case '-':
+            result=num1-num2;
+            cout<<"Result: "<<result<<endl;
+            break;
+        case '*':
+            result=num1*num2;
+            cout<<"Result: "<<result<<endl;
+            break;
+        case '/':
+            result=num1/num2;
+            cout<<"Result: "<<result<<endl;
+            break;
+        default:
+            cout<<"Error! Operator is not correct"<<endl;
+            break;
+    }
+    cout<<"****************************";
+
+
 }
