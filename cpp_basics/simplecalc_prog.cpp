1dc20edd4ef92b86627d72350e0a218f0fbd9df5
adityamengar <mengaraditya@gmail.com>
2026-08-10 23:15:37 +0530

    Console calc program

---
+++ cpp_basics/simplecalc_prog.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char op;
+    double num1;
+    double num2;
+    double result;
+    cout<<"-----CALCULATOR-----"<<endl;
+    cout<<"Enter either(+,-,*,/) : ";
+    cin>>op;
+}
