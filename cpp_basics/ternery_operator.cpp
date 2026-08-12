738d8322ce2511859ffb8b36a9d8e4fb46ad25ae
adityamengar <mengaraditya@gmail.com>
2026-08-12 21:56:47 +0530

    TERNARY OPERATOR EXAMPLE CODES

--- cpp_basics/ternery_operator.cpp
+++ cpp_basics/ternery_operator.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    //Ternary Operataor 
+    //Ternary Operataor ?: = replacement for if-else statement
+    //condition ? expression1 : expression2;
+    //int grade = 75;
+    //grade>=60 ? cout<<"You PASS! " : cout<<"You FAIL! ";
+    
+    //int number = 9;
+    //number%2 == 1 ? cout<<"ODD" : cout<<"EVEN";
+    //number%2 ? cout<<"ODD" : cout<<"EVEN";
+    
+    bool hungry = true;
+    hungry
+    return 0;
 }
