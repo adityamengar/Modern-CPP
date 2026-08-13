41821279e55f91a0f1a140d4b84c5dcc73fc390a
adityamengar <mengaraditya@gmail.com>
2026-08-13 19:56:58 +0530

    Logical operator (&)

---
+++ cpp_basics/logical_operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // && = check if conditions are true
+    // || = check if at least one condition is true
+    // ! = reverses the logical state of its operand
+    int temp;
+    cout<<"Enter the temperature in Celcius: ";
+    cin>>temp;
+
+    if(temp>0 && temp<30){
+        cout<<"The temperature is good today! ";
+
+    }
+    else{
+        cout<<"The temperature is bad today! ";
+    }
+return 0;
+}
