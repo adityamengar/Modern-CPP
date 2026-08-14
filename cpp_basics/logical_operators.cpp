5a36e1a9b00ce5617e12718173531786713712ff
adityamengar <mengaraditya@gmail.com>
2026-08-14 16:25:22 +0530

    ! Logical operator Eg's

--- cpp_basics/logical_operators.cpp
+++ cpp_basics/logical_operators.cpp
@@ -5,16 +5,24 @@ int main(){
     // || = check if at least one condition is true
     // ! = reverses the logical state of its operand
     int temp;
+    bool sunny=false;
     cout<<"Enter the temperature in Celcius: ";
     cin>>temp;
 
     //if(temp>0 && temp<30){  (OR)
     if(temp<=0 || temp>=30){
-        cout<<"The temperature is bad today! ";
+        cout<<"The temperature is bad today! "<<endl;
 
     }
     else{
-        cout<<"The temperature is good today! ";
+        cout<<"The temperature is good today! "<<endl;
+    }
+
+    if(!sunny){
+        cout<<"It is cloudy outside! "<<endl;
+    }
+    else{
+        cout<<"It is sunny Outside!"<<endl;
     }
 return 0;
 }
