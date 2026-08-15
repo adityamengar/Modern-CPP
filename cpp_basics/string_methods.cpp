2a83b20b984dca00656fe834f4b011158753620b
adityamengar <mengaraditya@gmail.com>
2026-08-15 23:47:26 +0530

    Empty string usecase

--- cpp_basics/string_methods.cpp
+++ cpp_basics/string_methods.cpp
@@ -4,11 +4,19 @@ int main(){
     string name;
     cout<<"Enter Your Name: ";
     getline(cin, name);
-    if(name.length()>12){
+    /*if(name.length()>12){
         cout<<"our Name Cannnot be over 12 characters";
     }
     else{
         cout<<"Welcome "<<name;
-    }
+    }*/
+   
+    if(name.empty()){
+    cout<<"You didn't Enter Your Name";
+
+   }
+   else{
+    cout<<"Hello "<<name;
+   }
     return 0;
 }
