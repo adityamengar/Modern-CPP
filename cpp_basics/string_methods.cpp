92d110208fa6476e68842594e5dbc774bf8740e6
adityamengar <mengaraditya@gmail.com>
2026-08-17 22:34:53 +0530

    string methods using .find;.erase

--- cpp_basics/string_methods.cpp
+++ cpp_basics/string_methods.cpp
@@ -18,9 +18,13 @@ int main(){
     cout<<"Hello "<<name;
    }*/
     //name.clear(); //clears the name
-    name.append("@gmail.com");
-    cout<<"Your Username is now: "<<name;
+    //name.append("@gmail.com");
     
-  
+    cout<<"Your Username is now: "<<name<<endl;
+    cout<<name.at(1)<<endl;  //in bracket it shows the index number 0,1,2,etc
+    //name.insert(0, "@");  //(index , what you want to insert at that specific index)
+    cout<<name.find(' ')<<endl;
+
+    name.erase(0,3); //(beginning index , ending index) it will remove from beginning to end index
     return 0;
 }
