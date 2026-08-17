62ba401623b36906eb18ab492eba291ffd3d6b97
adityamengar <mengaraditya@gmail.com>
2026-08-17 22:44:50 +0530

    While loops usecases

---
+++ cpp_basics/while_loops.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    while(name.empty()){
+        cout<<"Enter You Name: ";
+        getline(cin,name);
+    }
+    cout<<"hELLO "<<name<<endl;
+}
