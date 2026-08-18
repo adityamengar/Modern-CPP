de0f87b961a1f10297ca188197662e0d0ec07a00
adityamengar <mengaraditya@gmail.com>
2026-08-18 23:29:55 +0530

    ERRORS FIXED

--- cpp_basics/do_while_loop.cpp
+++ cpp_basics/do_while_loop.cpp
@@ -1,7 +1,14 @@
-#include<iostreamm>
+#include<iostream>
 using namespace std;
 int main(){
-    //do while loop = do somem block of coode first,
+    //do while loop = do some block of coode first,
     //                THEN repeat again if condition is TRUE
-     
+    int number;
+    do
+    {
+        cout<<"Enter a +ve NUMBER: ";
+        cin>>number;
+    }while(number<0);
+    cout<<"The NUMBER is : "<<number;
+    return 0;
 }
