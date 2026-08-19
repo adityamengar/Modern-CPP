df61f8d421cc8c911dbf0a7ad5176abaf36e7540
adityamengar <mengaraditya@gmail.com>
2026-08-19 23:52:25 +0530

    Increment Decrement uses

--- cpp_basics/for_loops.cpp
+++ cpp_basics/for_loops.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1; i<=10; i++){
+    for(int i=10; i>=0; i--){ //i++ means increment by one i+=2 means increment by 2
         cout<<i<<'\n';
     }
     cout<<"HAPPY NEW YEAR! \n";
