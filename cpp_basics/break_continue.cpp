762e40fe889d1c799842cbddfd71031379926234
adityamengar <mengaraditya@gmail.com>
2026-08-21 19:51:04 +0530

    ERRORS

--- cpp_basics/break_continue.cpp
+++ cpp_basics/break_continue.cpp
@@ -4,8 +4,9 @@ int main(){
     //break = break out of a loop
     //continue = skip current iteration
 
-    for(int i = 1; i <=20; i++){
-        if(i==13){
+    for(int i = 1; i <=20;i++){
+        if(i=13){
+            //break;
             continue;
         }
         cout<<i<<'\n';
