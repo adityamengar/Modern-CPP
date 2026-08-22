498b0cf01ca8255246087442976dfb342e0591ce
adityamengar <mengaraditya@gmail.com>
2026-08-22 22:31:23 +0530

    Process of random number

--- cpp_basics/random_no_generator.cpp
+++ cpp_basics/random_no_generator.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 using namespace std;
+int main(){
+    srand(time(NULL));
+    int num = (rand() % 6)+1;
+    //int num1 = (rand()%100)+1;
+    //int num2 = (rand()%5)+1;
+    cout<<num<<'\n';
+    //cout<<num1<<'\n';
+    //cout<<num2<<'\n';
+
+    return 0;
+}
