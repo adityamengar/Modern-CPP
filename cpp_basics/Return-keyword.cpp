96f400a3ea66c1ada0a465f77f8d9903b031441a
adityamengar <mengaraditya@gmail.com>
2026-08-25 23:22:46 +0530

    Using Variables and Intetgers for return

--- cpp_basics/Return-keyword.cpp
+++ cpp_basics/Return-keyword.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
+
+double square(double length);
+double cube(double length);
+
 int main(){
     //return = return a value back to te spot
     //          where you called the encompassing function
     double length = 6.0;
-    double area;
+    double area=square(length);
+    double volume=cube(length);
+    cout<<" Area : "<<area<<" cm^2\n";
+    cout<<" Volume : "<<volume<<"cm^3\n";
     return 0;
+}
+double square(double length){
+    return length*length;
+}
+double cube(double length){
+    return length*length*length;
 }
