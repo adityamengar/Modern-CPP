408a4877b812a50eba91ce17d5c0bfaa5c97bbc2
adityamengar <mengaraditya@gmail.com>
2026-08-25 23:17:52 +0530

    Created Functions and variables

---
+++ cpp_basics/Return-keyword.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //return = return a value back to te spot
+    //          where you called the encompassing function
+    double length = 6.0;
+    double area;
+    return 0;
+}
