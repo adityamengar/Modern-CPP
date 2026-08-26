9a073e1d1f469c57e56996aa834963639b427815
adityamengar <mengaraditya@gmail.com>
2026-08-26 23:50:17 +0530

    Final Results worked

--- cpp_basics/return_keywork_string.cpp
+++ cpp_basics/return_keywork_string.cpp
@@ -6,9 +6,12 @@ int main(){
     string firstName = "Aditya";
     string lastName = "Mengar";
     string fullName = concatString(firstName,lastName);
+    
+    cout<<"HELLO "<<fullName;
+    return 0;
 }
 
 string concatString(string string1, string string2){
-    
+    return string1 + " " + string2;
 }
 
