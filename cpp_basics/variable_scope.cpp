857b7fbb4a49124d17928668fc0737d7bb5f9dba
adityamengar <89826517+adityamengar@users.noreply.github.com>
2026-08-28 23:49:46 +0530

    Declaration

--- cpp_basics/variable_scope.cpp
+++ cpp_basics/variable_scope.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
+void printNum(int myNum);
 int main(){
 
     //local variablles = declared inside a function or block {}
     //Global Variable = declared outside of all functions
-    //check
+    int myNum=1;
+    printNum(myNum);
     return 0;
+}
+
+void printNum(int myNum){
+    cout<<myNum;
 }
