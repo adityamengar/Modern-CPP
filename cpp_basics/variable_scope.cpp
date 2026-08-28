2689755da97c8d2a57997d9cc61b6fecb66dab37
adityamengar <89826517+adityamengar@://github.com>
2026-08-28 23:17:53 +0530

    Global Variables

--- cpp_basics/variable_scope.cpp
+++ cpp_basics/variable_scope.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 
-    //llocal variablles = declared inside a function or block {}
+    //local variablles = declared inside a function or block {}
+    //Global Variable = declared outside of all functions
     return 0;
 }
