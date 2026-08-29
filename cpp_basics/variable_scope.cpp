a6c6936fde9e5e54f223e7d9fc047286088813dd
adityamengar <89826517+adityamengar@users.noreply.github.com>
2026-08-29 22:45:03 +0530

    Scope Resolution Op

--- cpp_basics/variable_scope.cpp
+++ cpp_basics/variable_scope.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 using namespace std;
-void printNum(int myNum);
+
+int myNum=3; //this is global variable declared outside 
+void printNum();
 int main(){
 
     //local variablles = declared inside a function or block {}
     //Global Variable = declared outside of all functions
-    int myNum=1;
-    printNum(myNum);
+
+    int myNum=1; //this is also local variable
+    printNum();
+    //cout<<myNum<<endl;
+    cout<<::myNum<<endl; //(::)This is scope resolution operator used to let the global variable use in it
     return 0;
 }
 
-void printNum(int myNum){
-    cout<<myNum;
+void printNum(){
+    int myNum = 2; //this is local variable
+    cout<<myNum<<endl;
 }
