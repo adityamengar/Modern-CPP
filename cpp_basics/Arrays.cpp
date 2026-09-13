8093033ba8ddc1605a831b72bfe02f0eb8b7a4da
adityamengar <mengaraditya@gmail.com>
2026-09-13 22:02:32 +0530

    Define

--- cpp_basics/Arrays.cpp
+++ cpp_basics/Arrays.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namspace std;
 int main(){
-    
+    //array= It is a data structure that can hold multiple values
+    //       values are accessed by an index number
+    //       "Kind of like a variable that holds multiple values" 
+    return 0;
 }
