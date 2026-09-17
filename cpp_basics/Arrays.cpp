ae6c965353f4510b35df7973a93bc096b591abd3
adityamengar <mengaraditya@gmail.com>
2026-09-17 23:28:09 +0530

    Array Example

--- cpp_basics/Arrays.cpp
+++ cpp_basics/Arrays.cpp
@@ -4,5 +4,6 @@ int main(){
     //array= It is a data structure that can hold multiple values
     //       values are accessed by an index number
     //       "Kind of like a variable that holds multiple values" 
+    string car = "TATA"
     return 0;
 }
