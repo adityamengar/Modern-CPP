263c85150e2328ce0f2bbaa582627d77abfce51a
adityamengar <mengaraditya@gmail.com>
2026-09-19 18:12:59 +0530

    CARS

--- cpp_basics/Arrays.cpp
+++ cpp_basics/Arrays.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
-using namspace std;
+using namespace std;
 int main(){
     //array= It is a data structure that can hold multiple values
     //       values are accessed by an index number
     //       "Kind of like a variable that holds multiple values" 
-    string car[] = {"TATA";"MUSTANG","DEFENDER"};
-    cout<<car;
+    string car[] = {"TATA","MUSTANG","MAHINDRA"};
+    cout<<car[0]<<"\n";
+    cout<<car[1]<<"\n";
+    cout<<car[2]<<"\n";
+
     return 0;
 }
