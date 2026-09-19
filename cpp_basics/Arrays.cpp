a4dc771c5841376e28562bb46b48a5a1556558cd
adityamengar <mengaraditya@gmail.com>
2026-09-19 18:15:37 +0530

    Compiler error

--- cpp_basics/Arrays.cpp
+++ cpp_basics/Arrays.cpp
@@ -5,6 +5,10 @@ int main(){
     //       values are accessed by an index number
     //       "Kind of like a variable that holds multiple values" 
     string car[] = {"TATA","MUSTANG","MAHINDRA"};
+    //Arrays Must always be of same data type
+    // LIKE here if you see in bracket{"Tata",1}, if we add number it will gve compiler error
+    
+    car[1]="BMW";
     cout<<car[0]<<"\n";
     cout<<car[1]<<"\n";
     cout<<car[2]<<"\n";
